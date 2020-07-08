c5461d0a1e8785a6829315a34609fd0287ca90d1
admin <mykola.rositskyi@zadeagency>
2020-07-08 15:45:57 +0300

    FAQ page init

--- src/modules/app/routes/routes.component.tsx
+++ src/modules/app/routes/routes.component.tsx
@@ -7,7 +7,7 @@ import * as styles from './routes.scss';
 import { useSelector } from 'react-redux';
 import { State } from '@app/store/state';
 import { Toast } from '@core/components';
-import { LearningApproach } from 'src/modules/pages';
+import { LearningApproach, Faq } from 'src/modules/pages';
 import { JobsList } from '@pages/jobs-list';
 import { JobDetails } from '@pages/job-details';
 import { ForCompanies } from '@pages/for-companies';
@@ -22,13 +22,13 @@ const Routes: React.FC = () => {
   return (
     <div className={styles.routes}>
       {isToastVisible && <Toast />}
-      <Route path='/profile' component={Profile} />
-      <Route path='/auth' component={Auth} />
-      <Route path='/uikit' component={Uikit} />
       <Switch>
         <Route path='/profile' component={Profile} />
         <Route path='/auth' component={Auth} />
         <Route path='/uikit' component={Uikit} />
+
+        {/* ROUTES */}
+        <Route path='/faq' component={Faq} />
         <Route exact path='/learning-approach' component={LearningApproach} />
         <Route path='/insights' component={Insights} />
         {/* JOB ROUTES */}
---
+++ src/modules/pages/faq/faq.component.tsx
@@ -0,0 +1,18 @@
+import * as React from 'react';
+import { FaqProps } from './faq.props';
+import * as styles from './faq.scss';
+
+/**
+ * Renders Faq
+ */
+const Faq: React.FC<FaqProps> = ({}) => {
+  return (
+    <div className={styles.faq}>
+      {/* content */}
+      {/* content */}
+      {/* content */}
+    </div>
+  );
+};
+
+export { Faq };
