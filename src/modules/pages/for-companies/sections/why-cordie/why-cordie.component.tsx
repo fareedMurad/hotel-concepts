3b00445d5cb058d4f6ba0fd9130a1ae4d084b714
admin <mykola.rositskyi@zadeagency>
2020-07-08 15:09:02 +0300

    fixed alias

--- src/modules/pages/for-companies/sections/why-cordie/why-cordie.component.tsx
+++ src/modules/pages/for-companies/sections/why-cordie/why-cordie.component.tsx
@@ -13,7 +13,7 @@ import { Icon, H1, H2, Paragraph } from '@core/components';
 const WhyCordie: React.FC<WhyCordieProps> = ({}) => {
   return (
     <div className={styles.whyCordie}>
-      <Icon name='home/abstract-1' />
+      <Icon name='learning-approach/abstract-1' />
       <H1>Why Kordie?</H1>
 
       <div className={styles.wrapper}>
