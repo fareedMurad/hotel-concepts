5dc9e1107967191b90c5fdab17f1c6f9eaf4cff9
tsybrukh.andrii <andrii.tsybrukh@jetsoftpro.com>
2020-07-10 13:06:07 +0200

    init program item

---
+++ src/modules/pages/programs-catalogue/components/program-item/program-item.component.tsx
@@ -0,0 +1,18 @@
+import * as React from 'react';
+import { ProgramItemProps } from './program-item.props';
+import * as styles from './program-item.scss';
+
+/**
+ * Renders ProgramItem
+ */
+const ProgramItem: React.FC<ProgramItemProps> = ({}) => {
+  return (
+    <div className={styles.programItem}>
+      {/* content */}
+      {/* content */}
+      {/* content */}
+    </div>
+  );
+};
+
+export { ProgramItem };
---
+++ src/modules/pages/programs-catalogue/components/program-item/program-item.props.ts
@@ -0,0 +1,6 @@
+/**
+ * Props
+ */
+type ProgramItemProps = {};
+
+export { ProgramItemProps };
