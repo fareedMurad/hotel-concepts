88702eee04856782a81e7ed9635e5baf2416dd46
TarasTaras36 <dr.tarasaleksandrov@gmail.com>
2020-07-13 15:09:07 +0300

    minor fixes

--- src/modules/pages/for-companies/sections/intro/intro.component.tsx
+++ src/modules/pages/for-companies/sections/intro/intro.component.tsx
@@ -10,26 +10,19 @@ import { ScrollButton } from '@core/components/scroll-button';
 const Intro: React.FC<IntroProps> = ({}) => {
   return (
     <div className={styles.intro}>
-      <div
-        style={{
-          backgroundImage: `url(${require('img/for-companies.png')})`
-        }}
-        className={styles.image}
-      >
-        <main className={styles.introContent}>
-          <H1 className={styles.introCaption}>
-            Hospitality education designed for impact
-          </H1>
-          <div className={styles.introDescription}>
-            Enforce your people and power your future
-          </div>
-          <Button className={styles.button}>
-            <div>Contact Us</div>
-            <div>&rarr;</div>
-          </Button>
-        </main>
-        <ScrollButton text='Scroll' className={styles.arrow} />
-      </div>
+      <main className={styles.introContent}>
+        <H1 className={styles.introCaption}>
+          Hospitality education designed for impact
+        </H1>
+        <div className={styles.introDescription}>
+          Enforce your people and power your future
+        </div>
+        <Button className={styles.button}>
+          <div>Contact Us</div>
+          <div>&rarr;</div>
+        </Button>
+      </main>
+      <ScrollButton text='Scroll' className={styles.arrow} />
     </div>
   );
 };
