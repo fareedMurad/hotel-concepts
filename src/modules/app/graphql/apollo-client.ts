dca7f02153940952ab7820eccb9a2e922cf8383d
Taras Pavliv <taraspavliv@MacBook-Pro-mac-2.local>
2020-07-23 10:58:42 +0300

    added env

--- src/modules/app/graphql/apollo-client.ts
+++ src/modules/app/graphql/apollo-client.ts
@@ -1,7 +1,7 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client';
 
 const client = new ApolloClient({
-  uri: `https://graphql.contentful.com/content/v1/spaces/${process.env.spaceId}`,
+  uri: `https://graphql.contentful.com/content/v1/spaces/${process.env.spaceId}/environments/dev`,
   headers: {
     authorization: `Bearer ${process.env.accessToken}`
   },
