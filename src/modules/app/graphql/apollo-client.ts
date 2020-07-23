c58b2084598eda94d027407621d8360369881caa
Taras Pavliv <taraspavliv@MacBook-Pro-mac-2.local>
2020-07-23 11:12:59 +0300

    fixes

--- src/modules/app/graphql/apollo-client.ts
+++ src/modules/app/graphql/apollo-client.ts
@@ -3,7 +3,7 @@ import { ApolloClient, InMemoryCache } from '@apollo/client';
 const client = new ApolloClient({
   uri: `https://graphql.contentful.com/content/v1/spaces/${process.env.spaceId}/environments/dev`,
   headers: {
-    authorization: `Bearer ${process.env.accessToken}`
+    authorization: `Bearer swkDaTwEiPt4P55HNC08DuKCEcSKKWDQcNy-xzMvs-I`
   },
   cache: new InMemoryCache()
 });
