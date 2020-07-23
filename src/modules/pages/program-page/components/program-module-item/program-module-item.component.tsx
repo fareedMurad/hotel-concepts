cc9054556c8a2b97331d4519b5276661cefa1203
Taras Pavliv <taraspavliv@MacBook-Pro-mac-2.local>
2020-07-23 10:12:42 +0300

    minor fixes pdf

--- src/modules/pages/program-page/components/program-module-item/program-module-item.component.tsx
+++ src/modules/pages/program-page/components/program-module-item/program-module-item.component.tsx
@@ -8,20 +8,21 @@ import { Button } from '@core/components';
  * Renders ProgramModuleItem
  */
 const ProgramModuleItem: React.FC<ProgramModuleItemProps> = ({ index, name, description, duration, pdf }) => {
-  const {weeks, houers} = duration
+  const { weeks, houers } = duration
   const [isOpened, toggleOpen] = React.useState(false);
   const updateOpenState = () => {
     toggleOpen(!isOpened);
   }
   const onModuleClick = () => {
-    window.open(require(`assets/pdf/${pdf}.pdf`));
+    window.open(pdf);
     return true;
   }
+  console.log(pdf)
   return (
     <div className={styles.programModuleItem}>
       <div className={styles.content}>
         <div className={styles.leftSide}>
-          <div onClick={updateOpenState} className={classNames(styles.indicator, {[styles.opened]: isOpened})}></div>
+          <div onClick={updateOpenState} className={classNames(styles.indicator, { [styles.opened]: isOpened })}></div>
           <div className={styles.nameBlock}>
             <div>{index + 1}.0</div>
             <div>{name}</div>
@@ -41,7 +42,7 @@ const ProgramModuleItem: React.FC<ProgramModuleItemProps> = ({ index, name, desc
           </Button>
         </div>
       </div>
-      <div className={classNames(styles.description, {[styles.opened]: isOpened})}>
+      <div className={classNames(styles.description, { [styles.opened]: isOpened })}>
         {description}
       </div>
       <div className={styles.hr}></div>
