02598c9164ca6bbd1fa3d16e1ad9741366d86759
TarasTaras36 <dr.tarasaleksandrov@gmail.com>
2020-07-30 12:44:34 +0300

    request test from aws

--- src/modules/core/components/footer/footer.component.tsx
+++ src/modules/core/components/footer/footer.component.tsx
@@ -9,6 +9,21 @@ import { Formik } from 'formik';
 import { Form } from '../form';
 import { Field } from '../field';
 import { gql, useQuery } from '@apollo/client';
+import axios from 'axios';
+
+import * as yup from 'yup';
+
+/**
+ * validation schema
+ */
+
+const validationSchema = yup.object<{ email: string }>().shape({
+  email: yup
+    .string()
+    .email()
+    .label('E-mail')
+    .required()
+});
 /**
  * query categories of programs
  */
@@ -63,11 +78,33 @@ const Footer: React.FC<FooterProps> = ({}) => {
   const [categories, setCategories] = React.useState([]);
   const { weprovideLinks, moreLinks, socials } = useFooterData();
   const { data, loading, error } = useQuery(CATEGORIES);
+
   React.useEffect(() => {
     if (!loading) {
       setCategories(data.courseCategoryCollection.items);
     }
   });
+
+  const response = axios
+    .post('https://us17.api.mailchimp.com/3.0/lists/6584bef461/members', {
+      headers: {
+        athorization: 'b39036c919aa93c2607bff916ca0b1e1-us17',
+        'content-type': 'application/json'
+      },
+      data: JSON.stringify({
+        email_address: 'somedude@gmail.com',
+        status: 'subscribed'
+      }),
+      auth: {
+        user: 'taras.pavliv@zade.agency',
+        pass: '$4zEUCrf2utSnt!'
+      }
+    })
+
+    .then(res => console.log(res))
+    .catch(err => console.log(err));
+
+  console.log(response);
 
   return (
     <div className={styles.footer} id='footer'>
@@ -88,6 +125,7 @@ const Footer: React.FC<FooterProps> = ({}) => {
             onSubmit={values => {
               console.log(values);
             }}
+            validationSchema={validationSchema}
           >
             {({ handleSubmit }) => (
               <Form handleSubmit={handleSubmit}>
