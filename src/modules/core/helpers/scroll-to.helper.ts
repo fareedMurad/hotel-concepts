69f6431fc41a1e3f1a781278150550374e933d40
TarasTaras36 <dr.tarasaleksandrov@gmail.com>
2020-09-01 17:54:04 +0300

    minor scroll fixes

--- src/modules/core/helpers/scroll-to.helper.ts
+++ src/modules/core/helpers/scroll-to.helper.ts
@@ -2,8 +2,8 @@ export const scrollTo = (id: string) => {
   const section = document.getElementById(id);
   if (section) {
     window.scrollBy({
-      top: section.offsetTop - window.pageYOffset - 60,
-      behavior: 'smooth',
+      top: section.offsetTop - window.pageYOffset - 89,
+      behavior: 'smooth'
     });
   }
-}
+};
