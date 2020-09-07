eed5187b5b908ff149969bbd8116016579e72287
Nazarii Baran <5796655-nazarii.baran@users.noreply.gitlab.com>
2020-09-07 16:50:50 +0300

    added otp page

--- src/modules/auth/pages/otp/otp.component.tsx
+++ src/modules/auth/pages/otp/otp.component.tsx
@@ -1,16 +1,35 @@
 import * as React from 'react';
 import { OtpProps } from './otp.props';
 import * as styles from './otp.scss';
+import OtpInput from 'react-otp-input';
+import { Label, Button } from '@core/components';
+import { useState } from 'react';
+import { useDispatch } from 'react-redux';
 
 /**
  * Renders Otp
  */
 const Otp: React.FC<OtpProps> = ({}) => {
+  const [value, setValue] = useState('');
+  const dispatch = useDispatch();
+
+  const handleSubmit = () => {
+    if (!value) return;
+
+    // dispatch(otp(value))
+    console.log(value);
+  };
+
   return (
     <div className={styles.otp}>
-      {/* content */}
-      {/* content */}
-      {/* content */}
+      <Label>Otp</Label>
+      <OtpInput
+        containerStyle={styles.input}
+        value={value}
+        onChange={(value: string) => setValue(value)}
+        numInputs={6}
+      />
+      <Button onClick={handleSubmit}>Submit</Button>
     </div>
   );
 };
