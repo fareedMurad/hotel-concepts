50f228b6afbc5e71a50aa7a84bacaf409f2051de
Volodymyr Didur <volodymyrdidur@MacBook-Pro-Volodymyr.local>
2020-09-22 17:24:31 +0300

    uncomment get user

--- src/modules/app/routes/routes.component.tsx
+++ src/modules/app/routes/routes.component.tsx
@@ -117,9 +117,9 @@ const Routes: React.FC = () => {
   const { mobile } = useMediaPoints();
   const dispatch = useDispatch();
 
-  // useEffect(() => {
-  //   dispatch(getUser());
-  // }, []);
+  useEffect(() => {
+    dispatch(getUser());
+  }, []);
 
   return (
     <Fragment>
