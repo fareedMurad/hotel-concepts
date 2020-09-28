ea137b6c50e0957337c178f05f02e321ad05c270
Volodymyr Didur <volodymyrdidur@MacBook-Pro-Volodymyr.local>
2020-09-28 17:31:19 +0300

    checkouts for authorized and not authorized users

--- src/modules/app/redux/checkout/saga.ts
+++ src/modules/app/redux/checkout/saga.ts
@@ -1,7 +1,9 @@
+import { CreateSession, CreateSessionItem } from '@app/models/fastspring';
 import { handleError } from '@general/store';
 import { Payload, Saga } from 'redux-chill';
-import { call, delay, put } from 'redux-saga/effects';
+import { call, delay, put, select } from 'redux-saga/effects';
 import { Context } from '../context';
+import { State } from '../state';
 import { checkout } from './actions';
 
 /**
@@ -14,7 +16,30 @@ class CheckoutSaga {
   @Saga(checkout)
   public *checkout(payload: Payload<typeof checkout>, { api, fs }: Context) {
     try {
-      yield call(fs.store.builder.push, payload, data => console.log(data));
+      const { authorized } = yield select((state: State) => state.auth);
+
+      const { products } = payload;
+
+      if (authorized) {
+        const items: CreateSessionItem[] = products.map(
+          ({ path, quantity }) => ({
+            product: path,
+            quantity
+          })
+        );
+        const response = yield call(api.checkout.createSession, {
+          products: items
+        });
+
+        const {
+          data: {
+            session: { id }
+          }
+        } = response;
+        yield call(fs.store.builder.checkout, id);
+      } else {
+        yield call(fs.store.builder.push, payload, data => console.log(data));
+      }
     } catch (error) {
       yield put(handleError(error.response.data.message));
     }
