8371885e9fe7d009f03d2e59c0ccb7de0f635309
Nazarii Baran <5796655-nazarii.baran@users.noreply.gitlab.com>
2020-10-07 15:38:19 +0300

    program results responsive fix

--- src/modules/pages/program-page/sections/program-results/program-results.component.tsx
+++ src/modules/pages/program-page/sections/program-results/program-results.component.tsx
@@ -1,11 +1,10 @@
+import { State } from '@app/redux/state';
+import { Icon } from '@core/components';
 import * as React from 'react';
-import { ProgramResultsProps } from './program-results.props';
-import * as styles from './program-results.scss';
-import { gql, useQuery } from '@apollo/client';
-import { Spinner } from '@core/components';
 import { useTranslation } from 'react-i18next';
 import { useSelector } from 'react-redux';
-import { State } from '@app/redux/state';
+import { ProgramResultsProps } from './program-results.props';
+import * as styles from './program-results.scss';
 
 /**
  * Renders ProgramResults
@@ -13,20 +12,19 @@ import { State } from '@app/redux/state';
 const ProgramResults: React.FC<ProgramResultsProps> = ({ data }) => {
   const { t } = useTranslation();
   const { language } = useSelector((state: State) => state.localization);
+  const { results } = data || {};
 
   return (
     <section id='results' className={styles.programResults}>
       <div className={styles.title}>{t('program-page.results')}</div>
-      <div className={styles.hr} />
-      {data?.results.map(item => (
-        <div key={item}>
-          <div className={styles.container}>
-            <div className={styles.icon} />
-            <div className={styles.result}>{item}</div>
+      <div className={styles.list}>
+        {results?.map((result, index) => (
+          <div className={styles.result} key={index}>
+            <Icon className={styles.resultCheck} name='mark-check' />
+            <div className={styles.resultCaption}>{result}</div>
           </div>
-          <div className={styles.hr} />
-        </div>
-      ))}
+        ))}
+      </div>
     </section>
   );
 };
