4f863adbf4b6e4b83e145dc832677613e0355f36
Volodymyr Didur <volodymyrdidur@MacBook-Pro-Volodymyr.local>
2020-10-08 17:57:19 +0300

    404 error on my account

--- src/modules/app/components/not-found/not-found.component.tsx
+++ src/modules/app/components/not-found/not-found.component.tsx
@@ -1,4 +1,7 @@
+import { State } from '@app/redux/state';
+import { Spinner } from '@core/components';
 import * as React from 'react';
+import { useSelector } from 'react-redux';
 import { NotFoundProps } from './not-found.props';
 import * as styles from './not-found.scss';
 
@@ -6,7 +9,12 @@ import * as styles from './not-found.scss';
  * Renders NotFound
  */
 const NotFound: React.FC<NotFoundProps> = ({}) => {
-  return <div className={styles.notFound}>404 page not found</div>;
+  const { location } = useSelector((state: State) => state.router);
+  return location?.pathname.includes('account/profile') ? (
+    <Spinner />
+  ) : (
+    <div className={styles.notFound}>404 page not found</div>
+  );
 };
 
 export { NotFound };
