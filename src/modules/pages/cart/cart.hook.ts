d8074c560b18635c5a7b99f61f96a4bbd753b675
Nazarii Baran <5796655-nazarii.baran@users.noreply.gitlab.com>
2020-10-26 10:59:08 +0200

    cart total added amount check

--- src/modules/pages/cart/cart.hook.ts
+++ src/modules/pages/cart/cart.hook.ts
@@ -1,10 +1,10 @@
+import { ContentType, CurrenciesesCharacters } from '@app/models/enum';
+import { getProducts } from '@app/redux/cart';
+import { checkout } from '@app/redux/checkout';
 import { State } from '@app/redux/state';
+import { isBackgroundWhite } from '@core/components/header/store';
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { cart as cartAction, getProducts } from '@app/redux/cart';
-import { ContentType, CurrenciesesCharacters } from '@app/models/enum';
-import { checkout } from '@app/redux/checkout';
-import { isBackgroundWhite } from '@core/components/header/store';
 
 const useCartData = () => {
   const dispatch = useDispatch();
@@ -95,8 +95,11 @@ const useCartData = () => {
 
   const summaryData = {
     total: products
-      ?.map(product => product.price)
-      ?.reduce((acc, cur) => acc + cur, 0),
+      ?.map(product => ({
+        price: product.price,
+        amount: selectedProducts.find(one => one.path == product.id).quantity
+      }))
+      ?.reduce((acc, cur) => acc + cur.price * cur.amount, 0),
     estimatedShipping: 'Free',
     estimatedTax: '0.00',
     onClick: () => {
@@ -111,19 +114,6 @@ const useCartData = () => {
   return {
     products: cartData,
     summaryData
-    // cartData,
-    // summaryData: {
-    //   total,
-    //   estimatedShipping: '',
-    //   estimatedTax: '0 $',
-    //   onClick: () => {
-    //     const items = cartData.map(item => ({
-    //       path: item.id,
-    //       quantity: item.quantity
-    //     }));
-    //     dispatch(checkout(items));
-    //   }
-    // }
   };
 };
 
