fcd59ed4c92ce8c56abc53ec1cabe4b771785baf
Mykola Rositskyy <mykola.rositskyi@zadeagency>
2020-11-04 14:09:34 +0200

    fixed triangle icon header navigation

--- src/modules/core/components/header/header-main/components/nav-title/nav-title.component.tsx
+++ src/modules/core/components/header/header-main/components/nav-title/nav-title.component.tsx
@@ -2,14 +2,25 @@ import * as React from 'react';
 import { NavTitleProps } from './nav-title.props';
 import * as styles from './nav-title.scss';
 import classNames from 'classnames';
+import { Icon } from '@core/components';
+import { useHeaderMainData } from '../../hooks';
 
 /**
  * Renders NavTitle
  */
-const NavTitle: React.FC<NavTitleProps> = ({ className, title }) => (
-  <div className={classNames(className, styles.navTitle)}>
-    {title} <span className={styles.arrow}>&#x25BE;</span>
-  </div>
-);
+const NavTitle: React.FC<NavTitleProps> = ({ className, title }) => {
+  const { whiteHeader, stickyHeader } = useHeaderMainData();
+  const blackTheme = whiteHeader || stickyHeader;
+
+  return (
+    <div className={classNames(className, styles.navTitle)}>
+      {title}{' '}
+      <Icon
+        className={classNames(styles.shape)}
+        name={blackTheme ? 'triangle-arr-b' : 'triangle-arr'}
+      />
+    </div>
+  );
+};
 
 export { NavTitle };
