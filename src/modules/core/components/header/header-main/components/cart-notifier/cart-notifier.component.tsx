ebfb721e18d69cb522feb7eaa0365d557a05e6a2
Nazarii Baran <5796655-nazarii.baran@users.noreply.gitlab.com>
2020-11-05 12:13:01 +0200

    cart styling fixed

--- src/modules/core/components/header/header-main/components/cart-notifier/cart-notifier.component.tsx
+++ src/modules/core/components/header/header-main/components/cart-notifier/cart-notifier.component.tsx
@@ -1,18 +1,10 @@
-import { ContentType } from '@account/pages/library/models';
 import { State } from '@app/redux/state';
-import { Button } from '@core/components';
-import { useClickOutside } from '@core/shared';
-import { navigate } from '@router/store';
 import * as React from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import { useSelector } from 'react-redux';
 import { animated } from 'react-spring';
-import { useCartNotifierData } from './cart-notifier.hook';
 import * as styles from './cart-notifier.scss';
-import { CartNotifierItem } from './components/cart-notifier-item';
 import { Dropdown } from './components/dropdown';
 import { Notification } from './components/notification';
-import { useRef } from 'react';
-import { cart } from '@app/redux/cart';
 
 /**
  * Renders CartNotifier
