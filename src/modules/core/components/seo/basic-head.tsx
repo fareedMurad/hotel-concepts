a95365e7af0df790f186c2347f037f5fb1f355ef
TarasTaras36 <dr.tarasaleksandrov@gmail.com>
2020-11-10 13:43:59 +0200

    change fav-icon

--- src/modules/core/components/seo/basic-head.tsx
+++ src/modules/core/components/seo/basic-head.tsx
@@ -1,16 +1,38 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Helmet from 'react-helmet';
 
 const BasicHead: React.FC = () => {
+  const [darkTheme, setDarkTheme] = useState({
+    matches: false,
+    media: '',
+    onchange: null
+  });
+
+  useEffect(() => {
+    setDarkTheme(window.matchMedia('(prefers-color-scheme: dark)'));
+    console.log(window.matchMedia('(prefers-color-scheme: dark)'));
+  }, []);
+
   return (
     <Helmet>
       <title>Kordie</title>
-      <link
-        rel='icon'
-        type='image/png'
-        sizes='72x72'
-        href={`${require('img/fav-primary/kordie_favicon_72_72.svg')}`}
-      />
+      {darkTheme.matches ? (
+        <link
+          rel='icon'
+          type='image/png'
+          sizes='72x72'
+          href={`${require('img/fav-secondary/kordie_favicon_72_72.svg')}`}
+          id='dark-scheme-icon'
+        />
+      ) : (
+        <link
+          rel='icon'
+          type='image/png'
+          sizes='72x72'
+          href={`${require('img/fav-primary/kordie_faviconP_72_72.svg')}`}
+          id='light-scheme-icon'
+        />
+      )}{' '}
     </Helmet>
   );
 };
