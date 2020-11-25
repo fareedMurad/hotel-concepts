1106f88b2b32c29e350b06dbd24491f25aaa42b6
TarasTaras36 <dr.tarasaleksandrov@gmail.com>
2020-11-25 12:56:52 +0200

    work in progress

--- src/modules/pages/learning-approach/sections/meet-kordie/meet-kordie.component.tsx
+++ src/modules/pages/learning-approach/sections/meet-kordie/meet-kordie.component.tsx
@@ -3,14 +3,19 @@ import { MeetKordieProps } from './meet-kordie.props';
 import * as styles from './meet-kordie.scss';
 import { Icon, H2, Paragraph, SectionTitle } from '@core/components';
 import { useTranslation } from 'react-i18next';
+import { useToggleAnimate } from '@pages/learning-approach/animation-wrap';
 
 /**
  * Renders MeetKordie
  */
 const MeetKordie: React.FC<MeetKordieProps> = ({}) => {
+  const ref = React.useRef(null);
   const { t } = useTranslation();
+  React.useEffect(() => {}, [ref]);
+  const { toggle } = useToggleAnimate(ref);
+
   return (
-    <div className={styles.meetKordie}>
+    <div className={styles.meetKordie} ref={ref}>
       <Icon name='abstract-1' />
       <SectionTitle className={styles.meetKordieTitle}>
         {t('learning-approach.meet-kordie.title')}
