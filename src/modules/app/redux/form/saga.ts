0ce4f4016fc3fd56e3e5bba5e6ad469b7d77fcec
TarasTaras36 <dr.tarasaleksandrov@gmail.com>
2020-11-25 13:12:43 +0200

    added nodal

--- src/modules/app/redux/form/saga.ts
+++ src/modules/app/redux/form/saga.ts
@@ -52,17 +52,18 @@ class FormSaga {
   ) {
     yield put(preloaderStart(Preloaders.sendForm));
     try {
-      console.log('fire');
       const response = yield call(
         api.form.sendSubscribeBetaSpecialOffer,
         payload
       );
-
       /**
        * Effects
        */
+
       const effects = [put(showModal(Modals.formResult))];
+      console.log(effects);
       yield all(effects);
+      yield put(closeModal(Modals.subscribe));
     } catch (error) {
       yield put(handleError(error.response.data.message));
     } finally {
