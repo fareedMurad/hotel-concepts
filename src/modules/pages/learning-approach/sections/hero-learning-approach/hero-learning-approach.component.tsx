e3c739cc50b6801942fed1664463ca1065883c15
TarasTaras36 <dr.tarasaleksandrov@gmail.com>
2020-11-26 15:57:53 +0200

    temp test

--- src/modules/pages/learning-approach/sections/hero-learning-approach/hero-learning-approach.component.tsx
+++ src/modules/pages/learning-approach/sections/hero-learning-approach/hero-learning-approach.component.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import { HeroProps } from './hero-learning-approach.props';
 import * as styles from './hero-learning-approach.scss';
-import { HeroTitle, HeroSubtitle, Icon } from '@core/components';
+import { HeroTitle, HeroSubtitle, Icon, Button } from '@core/components';
 import { ScrollButton } from '@core/components/scroll-button';
 import { useHeroLearningApproachData } from './hero-learning-approach.hook';
 import { useTranslation } from 'react-i18next';
@@ -17,29 +17,58 @@ const HeroLearningApproach: React.FC<HeroProps> = ({}) => {
   } = useHeroLearningApproachData();
   const { t } = useTranslation();
 
+  const [toggleImage, set] = React.useState(false);
+
   return (
     <div className={styles.hero}>
-      <LazyBackground
-        className={styles.image}
-        fullImageId={'wsal0kfBcI5dB1icnLUQu'}
-        reducedImageId={'33zlz89QUmqCUxfAEvgSFu'}
-      >
-        {/* <div className={styles.geometry}>
-          <Icon name={'geometry/learning-approach'} />
-        </div> */}
-        <main className={styles.heroContent}>
-          <HeroTitle className={styles.heroCaption}>
-            {t('learning-approach.hero.title')}
-          </HeroTitle>
-          <HeroSubtitle className={styles.heroDescription}>
-            {t('learning-approach.hero.description')}
-          </HeroSubtitle>
-        </main>
-        <ScrollButton
-          text={t('learning-approach.hero.scroll')}
-          className={styles.arrow}
-        />
-      </LazyBackground>
+      <Button className={styles.temp} onClick={() => set(!toggleImage)}>
+        {toggleImage
+          ? 'Change image current:[grouped]'
+          : 'Change image current:[with SVG]'}
+      </Button>
+      {toggleImage ? (
+        <LazyBackground
+          className={styles.image}
+          fullImageId={'wsal0kfBcI5dB1icnLUQu'}
+          reducedImageId={'33zlz89QUmqCUxfAEvgSFu'}
+        >
+          <main className={styles.heroContent}>
+            <HeroTitle className={styles.heroCaption}>
+              {t('learning-approach.hero.title')}
+            </HeroTitle>
+            <HeroSubtitle className={styles.heroDescription}>
+              {t('learning-approach.hero.description')}
+            </HeroSubtitle>
+          </main>
+          <ScrollButton
+            text={t('learning-approach.hero.scroll')}
+            className={styles.arrow}
+          />
+        </LazyBackground>
+      ) : (
+        <LazyBackground
+          className={styles.image}
+          fullImageId={'1Xh52D8yAy5jSkjVnrW8S1'}
+          reducedImageId={'7dcwD5Us9xoMyX6I9yZWt4'}
+        >
+          <div className={styles.geometry}>
+            <Icon name={'geometry/learning-approach'} />
+          </div>
+
+          <main className={styles.heroContent}>
+            <HeroTitle className={styles.heroCaption}>
+              {t('learning-approach.hero.title')}
+            </HeroTitle>
+            <HeroSubtitle className={styles.heroDescription}>
+              {t('learning-approach.hero.description')}
+            </HeroSubtitle>
+          </main>
+          <ScrollButton
+            text={t('learning-approach.hero.scroll')}
+            className={styles.arrow}
+          />
+        </LazyBackground>
+      )}
     </div>
   );
 };
