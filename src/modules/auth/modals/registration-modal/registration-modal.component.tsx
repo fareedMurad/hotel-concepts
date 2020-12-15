bb69370207cbf7f50b2afac3bade68dedaa90118
Mykola Rositskyy <mykola.rositskyi@zadeagency>
2020-12-15 18:16:38 +0200

    fixed animation for modal

--- src/modules/auth/modals/registration-modal/registration-modal.component.tsx
+++ src/modules/auth/modals/registration-modal/registration-modal.component.tsx
@@ -6,8 +6,9 @@ import { ModalHeader } from './components/modal-header';
 import { ModalLogin } from './components/modal-login';
 import { ModalSignup } from './components/modal-signup';
 import { closeModal } from '@ui/modal';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { useState } from 'react';
+import { State } from '@app/redux/state';
 
 /**
  * Renders RegistrationModal
@@ -15,9 +16,31 @@ import { useState } from 'react';
 const RegistrationModal: React.FC = () => {
   const dispatch = useDispatch();
   const [activeModal, setActiveModal] = useState<'login' | 'register'>('login');
-
+  const { active } = useSelector((state: State) => state.ui.modal);
+  /**
+   * SUPER
+   */
+  const [handleAnim, setHandleAnim] = useState(false);
+  React.useEffect(() => {
+    if (activeModal === 'register') {
+      setHandleAnim(true);
+    }
+    if (!active.includes('registration')) {
+      setHandleAnim(false);
+    }
+  }, [activeModal, active]);
+  const isRegisterModalActive = activeModal === 'register';
+  const preventAnimationReset =
+    isRegisterModalActive || handleAnim || !active.includes('registration');
+  /**
+   * COSTILE
+   */
   return (
-    <Modal id={Modals.registration} className={styles.registrationModal}>
+    <Modal
+      id={Modals.registration}
+      className={styles.registrationModal}
+      noReset={preventAnimationReset}
+    >
       <Preloader id={Preloaders.login}>
         <Preloader id={Preloaders.register}>
           <Icon
