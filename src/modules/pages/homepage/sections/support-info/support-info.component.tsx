f4cec50b568860bbf3bca14d67a6efcf3c50655d
TarasTaras36 <dr.tarasaleksandrov@gmail.com>
2021-02-25 16:19:03 +0200

    fix

--- src/modules/pages/homepage/sections/support-info/support-info.component.tsx
+++ src/modules/pages/homepage/sections/support-info/support-info.component.tsx
@@ -59,7 +59,7 @@ const SupportInfo: React.FC<SupportInfoProps> = ({}) => {
           </div>
         ))}
       </div>
-      {/* <SubscribeModal /> */}
+      <SubscribeModal />
     </section>
   );
 };
